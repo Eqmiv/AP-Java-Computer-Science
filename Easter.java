b91d0a6d8402cb574669847c327e9cf2de22fac0
Eqmiv <boybingmontemayoriv@gmail.com>
2025-09-04 15:05:14 +0000

    9/4/2025

--- Easter.java
+++ Easter.java
@@ -1,4 +1,15 @@
+// Eliazar Montemayor IV
+// Your program will calculate the date Easter falls on a given year
 public class Easter{
-    public static void main(String[] args)
+    public static void main(String[] args){
+    int y = 2025;
+    int a = y % 19;
+    int b = y / 100;
+    int c = y % 100;
+    int d = b / 4;
+    int e = b % 4;
+    int f = b + 8 /25;
+    int g = b - f + 1 / 3;
+    System.out.println("a = "+ a);
 
-}
+}}
